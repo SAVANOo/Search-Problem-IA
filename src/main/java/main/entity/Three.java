71cbab70b1ba86a37117dca138c974e47e2ba5ae
SAVANOo <savio_gato1000@hotmail.com>
2025-03-25 20:13:30 -0300

    Criando árvores

---
+++ src/main/java/main/entity/Three.java
@@ -0,0 +1,43 @@
+package main.entity;
+
+public class Three {
+    public Node oradea = new Node("oradea");
+
+    public Node zerind = new Node("zerind");
+
+    public Node arad = new Node("arad");
+
+    public Node sibiu = new Node("sibiu");
+
+    public Node timisoara = new Node("timisoara");
+
+    public Node lugoj = new Node("lugoj");
+
+    public Node mehadia = new Node("mehadia");
+
+    public Node drobeta = new Node("drobeta");
+
+    public Node craiova = new Node("craiova");
+
+    public Node riminicu_vilcea = new Node("riminicu vilcea");
+
+    public Node pitesti = new Node("pitesti");
+
+    public Node fagaras = new Node("fagaras");
+
+    public Node bucharest = new Node("bucharest");
+
+    public Node giurgiu = new Node("giurgiu");
+
+    public Node urziceni = new Node("urziceni");
+
+    public Node vaslui = new Node("vaslui");
+
+    public Node iasi = new Node("iasi");
+
+    public Node neamt = new Node("neamt");
+
+    public Node hirsova = new Node("hirsova");
+
+    public Node eforie = new Node("eforie");
+}
