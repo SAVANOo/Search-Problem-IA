1328551d3a2682c4afee5ef24c035136e8979d7d
SAVANOo <savio_gato1000@hotmail.com>
2025-03-25 20:12:23 -0300

    Adicionando vizinhos ao nó

--- src/main/java/main/entity/Node.java
+++ src/main/java/main/entity/Node.java
@@ -1,13 +1,27 @@
 package main.entity;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Node {
     private String name;
 
+    private List<Neighbor> neighborList;
+
     public Node(String name) {
         this.name = name;
+        this.neighborList = new ArrayList<Neighbor>();
     }
 
     public String getName() {
         return name;
+    }
+
+    public List<Neighbor> getNeighborList() {
+        return this.neighborList;
+    }
+
+    public void addNeighbor(Node neighbor, Integer distance) {
+        this.neighborList.add(new Neighbor(neighbor, distance));
     }
 }
