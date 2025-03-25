c58542c339d828547df0e0ee851c01806eb3a719
SAVANOo <savio_gato1000@hotmail.com>
2025-03-25 20:11:30 -0300

    Criando um nó

---
+++ src/main/java/main/entity/Node.java
@@ -0,0 +1,13 @@
+package main.entity;
+
+public class Node {
+    private String name;
+
+    public Node(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
