79c42343734ef558bbb98dd7c1f6dcb53937ca8a
SAVANOo <savio_gato1000@hotmail.com>
2025-03-31 00:00:19 -0300

    Adicionando busca de aprofundamento interativo

---
+++ src/main/java/main/algorithm/IDDFS.java
@@ -0,0 +1,28 @@
+package main.algorithm;
+
+import main.entity.Node;
+import main.src.ResultAdapter;
+
+public class IDDFS {
+
+    private static final int WITHOUT_LIMIT = -1;
+
+    public static ResultAdapter searchWithDeepLimit(Node initial, Node target, Integer depth) {
+        return search(initial, target, depth);
+    }
+
+    public static ResultAdapter search(Node initial, Node target) {
+        return search(initial, target, WITHOUT_LIMIT);
+    }
+
+    private static ResultAdapter search(Node initial, Node target, Integer maxDepth) {
+        Integer currentDepth = 0;
+
+        while (true) {
+            if (currentDepth >= maxDepth && maxDepth != WITHOUT_LIMIT) return null;
+
+            ResultAdapter result = DFS.searchWithDeepLimit(initial, target, currentDepth);
+            if (result != null) return result;
+        }
+    }
+}
