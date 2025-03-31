a106d66a69fbe9ea782c6990fc39a9ca1d7e5c10
SAVANOo <savio_gato1000@hotmail.com>
2025-03-30 22:46:27 -0300

    Criando mecanismo para calcular a profundidade da pesquisa

--- src/main/java/main/algorithm/DFS.java
+++ src/main/java/main/algorithm/DFS.java
@@ -8,17 +8,30 @@ import java.util.*;
 
 public class DFS {
 
+    public static ResultAdapter searchWithDeepLimit(Node initial, Node target, Integer maxDepth) {
+        return search(initial, target, maxDepth);
+    }
+
     public static ResultAdapter search(Node initial, Node target) {
+        return search(initial, target, -1);
+    }
+
+    private static ResultAdapter search(Node initial, Node target, Integer maxDepth) {
         Deque<Node> stack = new ArrayDeque<>();
-        List<Node> visited = new LinkedList<>();
+        Map<Node, Integer> depthMap = new HashMap<>();
         Map<Node, Node> childAndOwner = new HashMap<>();
+        List<Node> visited = new LinkedList<>();
 
         stack.push(initial);
         visited.add(initial);
+        depthMap.put(initial, 0);
 
         while (!stack.isEmpty()) {
             Node process = stack.pop();
+            int currentDepth = depthMap.get(process);
+
             if (process.equals(target)) break;
+            if (maxDepth != -1 && currentDepth >= maxDepth) continue;
 
             for (Neighbor neighbor : process.getNeighborList()) {
                 Node next = neighbor.getNode();
@@ -27,6 +40,7 @@ public class DFS {
                 childAndOwner.put(next, process);
                 visited.add(next);
                 stack.push(next);
+                depthMap.put(next, currentDepth + 1);
             }
         }
 
